03a0fe7ca45a8cce9abd3f83f3d3b71181966c34
whereiszhenya <whereiszhenya@MacBook-Pro-whereiszhenya.local>
2024-05-19 01:10:09 +0500

    up

--- src/App.tsx
+++ src/App.tsx
@@ -4,62 +4,71 @@ import { useMainContract } from "./hooks/useMainContract";
 import { useTonConnect } from "./hooks/useTonConnect";
 import { fromNano } from "ton-core";
 import WebApp from '@twa-dev/sdk'
+import { useState } from "react";
+import ShitcoinImage from './assets/Shitcoin.png';
 
 function App() {
-  const {
-    contract_address,
-    counter_value,
-    contract_balance,
-    sendIncrement,
-    sendDeposit,
-    sendWithdrawalRequest
-  } = useMainContract();
 
-  const {connected} = useTonConnect();
+  const [counter_value, setCounterValue] = useState(0);
+
+  const incrementCounter = () => {
+    setCounterValue(counter_value + 1);
+  };
+
+  const [score, setScore] = useState(86120);
+  const maxEnergy = 172800;
+  const currentEnergy = maxEnergy;
+  const timeRemaining = "14h 28m 3s";
+
+  const { connected } = useTonConnect();
 
   const showAlert = () => {
     WebApp.showAlert("Hey there!");
   };
-  
+
   return (
-    <div>
-      <div>
+    <div className="app">
+      {/* <div>
         <TonConnectButton />
-      </div>
+      </div> */}
       <div>
         <div className='Card'>
-          <b>{WebApp. platform}</b>
-          <br />
-          <b>Our contract Address</b>
+          {/* <b>{WebApp. platform}</b>
+          <br /> */}
+          {/* <b>Our contract Address</b>
           <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
           <b>Our contract Balance</b>
           {contract_balance && (
             <div className='Hint'>{fromNano(contract_balance)}</div>
-          )}
+          )} */}
         </div>
 
         <div className='Card'>
-          <b>Counter Value</b>
-          <div>{counter_value ?? "Loading..."}</div>
+          <h1>Shitcoin</h1>
+          <div className="score-section">
+            <h2>{counter_value}</h2>
+          </div>
+          <div className="icon-section">
+            <img onClick={incrementCounter} src={ShitcoinImage} alt="icon" className="icon" />
+          </div>
+          <div className="actions-section">
+            <button className="action-btn">💰 Earn</button>
+            <button className="action-btn">🚀 Boosts</button>
+            <button className="action-btn">👬 Friends</button>
+          </div>
         </div>
-
+      {/* 
         <a
           onClick={() => {
             showAlert();
           }}
         >
           Show Alert
-        </a>
+        </a> */}
 
-        <br />
+      <br />
 
-        {connected && (
-          <a onClick={() => sendIncrement()}>Increment by 1</a>
-        )}
-
-        <br/>
-
-        {connected && (
+      {/* {connected && (
            <a onClick={() => sendDeposit()}>Request deposit of 0.1 ton</a>
         )}
 
@@ -67,9 +76,9 @@ function App() {
 
         {connected && (
           <a onClick={() => sendWithdrawalRequest()}>Request withdraw of 0.1 ton</a>
-        )}
-      </div>
+        )} */}
     </div>
+    </div >
   );
 }
 
